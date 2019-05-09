f2444d66b50b7fed854dc52a18782c3ab38a46a0
Jurica Brekalo Strbic <Jurica.Brekalo.Strbic@gmail.com>
2019-05-09 14:28:50 +0200

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -2,55 +2,59 @@
  * index.js
  * - All our useful JS goes here, awesome!
  */
- const HORIZONTAL_LINE = 135;
+ const HORIZONTAL_LINE = 140;
 
 console.log("JavaScript is amazing!");
 document.addEventListener("keydown", moveJura);
 let jura = document.getElementById("jurica");
-console.log("hehe",parseInt(getComputedStyle(jura).bottom));
+console.log("hehe",parseInt(getComputedStyle(jura).top));
 
 let x=0;
 let y=0;
+let jump_direction=1;
+let jump_interval = 1;
 
 function moveJura(event){
   let jura = document.getElementById("jurica");
-  switch(event.key){
-    case "ArrowLeft":
-      if(parseInt(getComputedStyle(jura).top) > HORIZONTAL_LINE){
-        x = parseInt(getComputedStyle(jura).left)-5;
-        jura.style.left = `${x}px`;
-      }
-    break;
-      case "ArrowRight":
-      if(parseInt(getComputedStyle(jura).top) > HORIZONTAL_LINE){
-        x = parseInt(getComputedStyle(jura).left)+5;
-        jura.style.left = `${x}px`;
-      }
-    break;
-      case "ArrowUp":
-      if(parseInt(getComputedStyle(jura).top) > HORIZONTAL_LINE){
-        jump(20);
-      }
-    break;
-      case "ArrowDown":
-      if(parseInt(getComputedStyle(jura).top) > HORIZONTAL_LINE){
-        y = parseInt(getComputedStyle(jura).top)+5;
-        jura.style.top = `${y}px`;
-      }
-    break;
-  }
+
+    switch(event.key){
+      case "ArrowLeft":
+          x = parseInt(getComputedStyle(jura).left)-5;
+          jura.style.left = `${x}px`;
+      break;
+        case "ArrowRight":
+          x = parseInt(getComputedStyle(jura).left)+5;
+          jura.style.left = `${x}px`;
+      break;
+        case "ArrowUp":
+        if(parseInt(getComputedStyle(jura).top) === HORIZONTAL_LINE && jump_interval === 1){
+          console.log("starting jump",parseInt(getComputedStyle(jura).top));
+          jump_interval = setInterval(jump, 20);
+        }
+      break;
+        case "ArrowDown":
+        if(parseInt(getComputedStyle(jura).top) >= HORIZONTAL_LINE){
+          y = parseInt(getComputedStyle(jura).top)+5;
+          jura.style.top = `${y}px`;
+        }
+      break;
+    }
 }
 
-function jump(counter, pauseInterval){
-  while(counter>0){
-    if (counter>9){
-      y = parseInt(getComputedStyle(jura).top)-5;
-      jura.style.top = `${y}px`;
-    }
-    if (counter<10){
-      y = parseInt(getComputedStyle(jura).top)+5;
-      jura.style.top = `${y}px`;
-    }
-    counter--;
+function jump(){
+  if (parseInt(getComputedStyle(jura).top) < HORIZONTAL_LINE - 70){
+      jump_direction = -1;
+  }
+  y = parseInt(getComputedStyle(jura).top) - 5 * jump_direction;
+  jura.style.top = `${y}px`;
+
+  if (parseInt(getComputedStyle(jura).top) > HORIZONTAL_LINE){
+    y = parseInt(getComputedStyle(jura).top)-5;
+    jura.style.top = `${y}px`;
+    console.log("hehe",parseInt(getComputedStyle(jura).top));
+    jump_direction = 1;
+    clearInterval(jump_interval);
+    jump_interval=1;
+    console.log("clear_interval",jump_interval);
   }
 }
